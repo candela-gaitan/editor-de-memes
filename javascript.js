170410a6d177b72282004a66cb7f073fc7ae7c3d
Candela Gaitán <candxla@gmail.com>
2021-05-08 18:32:12 -0300

    add switch darkmode and aside

--- javascript.js
+++ javascript.js
@@ -1,1 +1,30 @@
+const imagePanelButton = document.getElementById("image-panel-button");
+const textPanelButton = document.getElementById("text-panel-button");
+const darkModeButton = document.getElementById("darkmode-button");
+const lightModeButton = document.getElementById("lightmode-button");
+const imagePanel = document.getElementById("image-panel");
+const textPanel = document.getElementById('text-panel');
+const styleSheet = document.getElementById('style-sheet');
+
+textPanelButton.addEventListener('click', () => {
+    imagePanel.style.display = "none";
+    textPanel.style.display = "block";
+})
+
+imagePanelButton.addEventListener('click', () => {
+    textPanel.style.display = "none";
+    imagePanel.style.display = "block";
+})
+
+lightModeButton.addEventListener('click', () => {
+    lightModeButton.style.display = "none";
+    darkModeButton.style.display = "block";
+    styleSheet.setAttribute('href', 'lightmode.css');
+})
+
+darkModeButton.addEventListener('click', () => {
+    darkModeButton.style.display = "none";
+    lightModeButton.style.display = "block";
+    styleSheet.setAttribute('href', 'darkmode.css');
+})
 
