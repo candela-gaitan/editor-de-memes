75a3b71991eb40fbc6bc23458137e4f04222e99a
Candela Gaitán <candxla@gmail.com>
2021-05-13 17:00:15 -0300

    add some functions to text panel

--- javascript.js
+++ javascript.js
@@ -1,10 +1,9 @@
+/* Cambio de panel */
+
 const imagePanelButton = document.getElementById("image-panel-button");
 const textPanelButton = document.getElementById("text-panel-button");
-const darkModeButton = document.getElementById("darkmode-button");
-const lightModeButton = document.getElementById("lightmode-button");
 const imagePanel = document.getElementById("image-panel");
 const textPanel = document.getElementById('text-panel');
-const styleSheet = document.getElementById('style-sheet');
 
 textPanelButton.addEventListener('click', () => {
     imagePanel.style.display = "none";
@@ -15,6 +14,12 @@ imagePanelButton.addEventListener('click', () => {
     textPanel.style.display = "none";
     imagePanel.style.display = "block";
 })
+
+/* Modo oscuro / Modo claro */
+
+const darkModeButton = document.getElementById("darkmode-button");
+const lightModeButton = document.getElementById("lightmode-button");
+const styleSheet = document.getElementById('style-sheet');
 
 lightModeButton.addEventListener('click', () => {
     lightModeButton.style.display = "none";
@@ -27,4 +32,83 @@ darkModeButton.addEventListener('click', () => {
     lightModeButton.style.display = "block";
     styleSheet.setAttribute('href', 'darkmode.css');
 })
+
+/* Panel de texto */
+
+const topText = document.getElementById("top-text");
+const topTextInput = document.getElementById("top-text-input");
+
+const topTextFunction = () => {
+    topTextInput.addEventListener('input', () => {
+        topText.innerText = topTextInput.value;
+    })   
+}
+
+topTextFunction();
+
+const bottomText = document.getElementById("bottom-text");
+const bottomTextInput = document.getElementById("bottom-text-input");
+
+const bottomTextFunction = () => {
+    bottomTextInput.addEventListener('input', () => {
+        bottomText.innerText = bottomTextInput.value;
+    })   
+}
+
+bottomTextFunction();
+
+const topTextCheckbox = document.getElementById("no-top-text-checkbox");
+const bottomTextCheckbox = document.getElementById("no-bottom-text-checkbox");
+
+const noTopText = () => {
+    if (topTextCheckbox.checked === true) {
+        topText.style.display = "none";
+    } else {
+        topText.style.display = "block";
+    }
+}
+
+const noBottomText = () => {
+    if (bottomTextCheckbox.checked === true) {
+        bottomText.style.display = "none";
+    } else {
+        bottomText.style.display = "block";
+    }
+}
+
+const fontOptions = document.getElementById("font-options");
+
+const selectFont = () => {
+    if (fontOptions.value == "arial") {
+        topText.style.fontFamily = "Arial";
+        bottomText.style.fontFamily = "Arial";
+    } else if (fontOptions.value == "arialBlack") {
+        topText.style.fontFamily = "Arial Black";
+        bottomText.style.fontFamily = "Arial Black";
+    } else if (fontOptions.value == "american") {
+        topText.style.fontFamily = "American Typewriter";
+        bottomText.style.fontFamily = "American Typewriter"; 
+    } else if (fontOptions.value == "andale") {
+        topText.style.fontFamily = "Andale Mono";
+        bottomText.style.fontFamily = "Andale Mono";
+    } else if (fontOptions.value == "comicSans") {
+        topText.style.fontFamily = "Comic Sans MS";
+        bottomText.style.fontFamily = "Comic Sans MS";
+    } else if (fontOptions.value == "helvetica") {
+        topText.style.fontFamily = "Helvetica";
+        bottomText.style.fontFamily = "Helvetica";
+    } else if (fontOptions.value == "impact") {
+        topText.style.fontFamily = "Impact";
+        bottomText.style.fontFamily = "Impact";
+    } else if (fontOptions.value == "verdana") {
+        topText.style.fontFamily = "Verdana";
+        bottomText.style.fontFamily = "Verdana";
+    } else if (fontOptions.value == "TNR") {
+        topText.style.fontFamily = "Times New Roman";
+        bottomText.style.fontFamily = "Times New Roman";
+    }
+}
+
+selectFont();
+
 
